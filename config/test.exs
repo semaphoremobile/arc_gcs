7683ee7ac4f49a916c6b6a2c518fe96303ab4944
technicalcapt <technicalcapt@gmail.com>
2019-06-03 18:43:46 +0700

    try goth system config

--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :arc, :bucket, System.get_env("ARC_TEST_BUCKET")
 
-config :goth, json: System.get_env("GOOGLE_CREDENTIAL")
+config :goth, json: {:system, "GOOGLE_CREDENTIAL"}
