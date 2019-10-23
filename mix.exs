caef1d8c75a8f78b90156ed01e869d28f4e2d3e8
Jeff Owens <dmin7b5@gmail.com>
2019-10-23 12:45:56 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -38,11 +38,11 @@ defmodule Arc.Storage.GCS.Mixfile do
 
   defp deps do
     [
-      {:arc, "~> 0.11.0"},
+      {:arc, "~> 0.8"},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.6", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:goth, "~> 1.1.0"},
+      {:goth, "~> 1.0"},
       {:sweet_xml, "~> 0.6"}
     ]
   end
